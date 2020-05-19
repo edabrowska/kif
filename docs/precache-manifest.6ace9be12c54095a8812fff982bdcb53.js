1d37d6106f08c9ce384d15c3aa39df56542013c6
Edyta Dąbrowska <editt.dabrowska@gmail.com>
2020-05-19 18:14:17 +0200

    create docs folder for github pages

---
+++ docs/precache-manifest.6ace9be12c54095a8812fff982bdcb53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0de85d1ee43296d4e8799b0e993caa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65f96df6c1863046ae20",
+    "url": "/static/js/2.f5704e6b.chunk.js"
+  },
+  {
+    "revision": "ba1025076cbf76da940463b192f1c371",
+    "url": "/static/js/2.f5704e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357fec1b53212be1ea46",
+    "url": "/static/js/main.2c49e130.chunk.js"
+  },
+  {
+    "revision": "9ffad8cb3e1b1cbfdb99",
+    "url": "/static/js/runtime-main.8f7bf538.js"
+  }
+]);
